6219c06667259b7645accaeb24c2723944187257
FranchiniMae <fmae.mercado@gmail.com>
2016-04-06 14:52:49 -0700

    Edits to show only one bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -180,11 +180,10 @@ HomeController.$inject = ["$http", "Account", "$scope"]; // minification protect
 function HomeController ($http, Account, $scope) {
 
   $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.series = ['Series A'];
 
   $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    [65, 59, 80, 81, 56, 55, 40]
   ];
 
   var vm = this;
